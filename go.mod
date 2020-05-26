47bdcfa1f34216b5f43d2d71b833e7435a1117d3
Leandro Rosa <leandrorosa85@gmail.com>
2020-05-26 17:38:39 +0200

    clean up

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/leandrorosa/mkvr
+
+go 1.14
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/bas24/googletranslatefree v0.0.0-20170719053803-07873a6de396
+	github.com/headzoo/surf v1.0.0 // indirect
+	github.com/headzoo/ut v0.0.0-20181013193318-a13b5a7a02ca // indirect
+	github.com/k3a/html2text v0.0.0-20191003111652-62431c4a3ba5
+	github.com/robertkrimen/otto v0.0.0-20191219234010-c382bd3c16ff // indirect
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+	gopkg.in/headzoo/surf.v1 v1.0.0
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+)
--- main.go
+++ main.go
@@ -14,18 +14,16 @@ import (
 func main() {
 	println("Searching for "+os.Args[1])
 	html := callKvr(os.Args[1])
-	parsedHtml := parseHTML(html)
-	fmt.Println(translate(parsedHtml))
+	parsedHTML := parseHTML(html)
+	fmt.Println(translate(parsedHTML))
 }
 
 func callKvr(kvrNumber string) string {
 	form := url.Values{}
 	form.Add("zapnummer", kvrNumber)
-	form.Add("pbAbfragen", "information desk")
 
 	req, _ := http.NewRequest("POST", "https://www17.muenchen.de/EATWebSearch/Auskunft", strings.NewReader(form.Encode()))
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Add("Cookie", "JSESSIONID=9C7440328C7D6097FCA92636B4531D08; _et_coid=c9c5697e8b9086bf8846dfc164c3c32e")
 
 	hc := http.Client{}
 	resp, _ := hc.Do(req)
@@ -38,8 +36,8 @@ func callKvr(kvrNumber string) string {
 	return string(bytes)
 }
 
-func parseHTML(plainHtml string) string {
-	return html2text.HTML2Text(plainHtml)
+func parseHTML(plainHTML string) string {
+	return html2text.HTML2Text(plainHTML)
 }
 
 func translate(text string) (string, error) {
